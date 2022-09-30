c4c35d7ae6c3abc80b6281f328300653c88843b0
Wuletaw Wonte <wuletawwonte@gmail.com>
2022-09-30 19:48:59 +0300

    Revert back email creads

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,14 +72,15 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
 
   config.action_mailer.smtp_settings = {
-    :address => 'smtp.gmail.com',
-    :port => 587,
-    :domain => 'helpjuice-interview.herokuapp.com',
-    :user_name => Rails.application.credentials.dig(:email, :username),
-    :password => Rails.application.credentials.dig(:email, :password),
-    :authentication => :plain,
-    :enable_starttls_auto => true  
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'helpjuice-interview.herokuapp.com',
+    user_name:            Rails.application.credentials.dig(:email, :username),
+    password:             Rails.application.credentials.dig(:email, :password),
+    authentication:       'plain',
+    enable_starttls_auto: true
   }
+
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
