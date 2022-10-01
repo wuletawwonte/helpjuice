689ee80191e60c0916a6841d72636b2807ea402e
Wuletaw Wonte <wuletawwonte@gmail.com>
2022-10-01 05:42:59 +0300

    Create after signout path

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+
+  protected
+    def after_sign_out_path_for(_resource)
+      new_user_session_path
+    end
 end
