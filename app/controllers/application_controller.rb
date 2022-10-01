1f389dc54c9bbfcf513de179e237a82a5d41c314
Wuletaw Wonte <wuletawwonte@gmail.com>
2022-10-01 10:57:42 +0300

    Create header partial and install font-awesome-rails gem

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,7 @@ class ApplicationController < ActionController::Base
   end
 
   def after_sign_in_path_for(_resource)
-    posts_index_path 
+    root_path
   end
 
   def update_allowed_parameters
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root "posts#index"
   devise_for :users
-  # Defines the root path route ("/")
+
   get "home/index"
 end
