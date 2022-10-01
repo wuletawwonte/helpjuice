6b89ba5ff87f5baf81fef0ae33edd5e349e77af3
Wuletaw Wonte <wuletawwonte@gmail.com>
2022-10-01 17:07:26 +0300

    Prevent new line of enter key

--- app/javascript/new-post.js
+++ app/javascript/new-post.js
@@ -1,20 +1,38 @@
+const PLACEHOLDER = "Type / for blocks, @ to link docs or poeple";
+
 const newPost = document.getElementById('newpost');
 const title = document.getElementById('title');
 const currentInput = document.createElement("input");
 
 title.addEventListener('keyup', (e) => {
   if(e.key === "Enter") {
-    console.log("Enter is pressed");
     currentInput.focus();
   }
 })
 
 currentInput.type = "text";
-currentInput.placeholder = "Type / for blocks, @ to link docs or poeple";
-
-currentInput.addEventListener('focus', () => {
-  currentInput.value = "What";
-
-});
+currentInput.placeholder = PLACEHOLDER;
 
 newPost.appendChild(currentInput);
+
+currentInput.addEventListener('keyup', (e) => {
+  if(e.key === "Enter" && currentInput.value === "/1") {
+    const newEl = document.createElement('h1');
+    newEl.contentEditable = true;
+    newEl.addEventListener('keypress', (e) => {
+      if(e.key === "Enter") {
+        e.preventDefault();
+        currentInput.focus();
+      }
+    })    
+    newPost.insertBefore(newEl, currentInput);
+    resetField();
+    newEl.focus();
+  }
+});
+
+function resetField() {
+  currentInput.value = "";
+  currentInput.placeholder = "";
+}
+
