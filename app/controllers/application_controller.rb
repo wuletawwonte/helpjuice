f386ba42ecebdee66c256d8c49b5eca54242d256
Wuletaw Wonte <wuletawwonte@gmail.com>
2022-10-01 08:07:43 +0300

    Add auth form styling for authentication forms

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,14 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :update_allowed_parameters, if: :devise_controller?
 
   protected
     def after_sign_out_path_for(_resource)
       new_user_session_path
+    end
+
+    def update_allowed_parameters
+      devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:first_name, :last_name, :email, :password, :password_confirmation) }
+      devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:first_name, :last_name, :email, :password, :current_password) }
     end
 end
