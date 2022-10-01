7f28294b3464634dcee299719baa3aa6185c7021
Wuletaw Wonte <wuletawwonte@gmail.com>
2022-10-01 09:00:30 +0300

    Run migration to add the trackable migration to the database

--- db/migrate/20221001055819_add_devise_trackable_columns_to_users.rb
+++ db/migrate/20221001055819_add_devise_trackable_columns_to_users.rb
@@ -1,6 +1,6 @@
 class AddDeviseTrackableColumnsToUsers < ActiveRecord::Migration[7.0]
   def change
-    add_column :users, :sign_in_count, :integer
+    add_column :users, :sign_in_count, :integer, default: 0, null: false
     add_column :users, :current_sign_in_at, :datetime
     add_column :users, :last_sign_in_at, :datetime
     add_column :users, :current_sign_in_ip, :string
