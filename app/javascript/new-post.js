ad01467e5d592f82f425691618e3111e0d5c560f
Wuletaw Wonte <wuletawwonte@gmail.com>
2022-10-02 22:12:28 +0300

    Remove writing prevention on space

--- app/javascript/new-post.js
+++ app/javascript/new-post.js
@@ -24,7 +24,7 @@ currentInputContainer.appendChild(inputSelector.getSelector());
 newPost.appendChild(currentInputContainer);
 
 currentInput.addEventListener('keydown', (e) => {
-  if(e.code === "Space" && currentInput.value === "/1") {    
+  if(e.code !== "Space" && currentInput.value === "/1") {    
     e.preventDefault();
     const headerOne = createHeaderOne();
     newPost.insertBefore(headerOne, currentInputContainer);
