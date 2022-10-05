726d44275ccae7977ece06d40e532ef2498b7109
Wuletaw Wonte <wuletawwonte@gmail.com>
2022-10-05 20:34:24 +0300

    Fix yarn problems from the host machine

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,24 +41,6 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-
-  config.action_mailer.delivery_method = :smtp
-  
-  config.action_mailer.smtp_settings = {
-      address:              'smtp.gmail.com',
-      port:                 587,
-      domain:               'locahost:3000',
-      user_name:            Rails.application.credentials.dig(:email, :username),
-      password:             Rails.application.credentials.dig(:email, :password),
-      authentication:       'plain',
-      enable_starttls_auto: true
-    }
-    
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default :charset => "utf-8"
-
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
